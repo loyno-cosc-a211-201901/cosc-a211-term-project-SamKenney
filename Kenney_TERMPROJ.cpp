51f10c1450dea0ee277b8f4e77fe22f5e501bd56
SamKenney <47219375+SamKenney@users.noreply.github.com>
2019-12-06 14:30:59 -0600

    Update Kenney_TERMPROJ.cpp
    here

--- Kenney_TERMPROJ.cpp
+++ Kenney_TERMPROJ.cpp
@@ -4,41 +4,35 @@ using namespace std;
  
 const int NUMBER_OF_NOTES = 7; //117
 const int NUMBER_OF_TIMES = 7;
-const double NOTE_E5 = 659.25;
-const double NOTE_FS5 = 739.99;
-const double NOTE_CS4 = 277.18;
-const double NOTE_A5 = 880.00;
-const double NOTE_GS5 = 830.61;  
-const double NOTE_D4 = 293.66;
-const double NOTE_E4 = 329.63;
-const double NOTE_B4 = 493.88;
-const double NOTE_B5 = 987.77;
+const int NOTE_E5 = 659.25;
+const int NOTE_FS5 = 739.99;
+const int NOTE_CS4 = 277.18;
+const int NOTE_A5 = 880.00;
+const int NOTE_GS5 = 830.61;  
+const int NOTE_D4 = 293.66;
+const int NOTE_E4 = 329.63;
+const int NOTE_B4 = 493.88;
+const int NOTE_B5 = 987.77;
 int play;
  
  
  
-  
+void playSong (int freqArray [], int timeArray[], int NUMBER_OF_NOTES);
  
 int main(){
     cout << "Welcome to the Christmas Music Box!" << endl;
     
-    double freqArray[NUMBER_OF_NOTES] = { NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_CS4, NOTE_A5, NOTE_FS5, NOTE_E5 };
+    int  freqArray[NUMBER_OF_NOTES] = { NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_CS4, NOTE_A5, NOTE_FS5, NOTE_E5 };
     
-    double timeArray[NUMBER_OF_TIMES] = {.004, .007, .007, .007, .007, .007, .007, };
+    int  timeArray[NUMBER_OF_TIMES] = { 4, 7, 7, 7, 7, 7, 7, };
  
-    Beep(freqArray, NUMBER_OF_NOTES);
- 
-    Beep(timeArray, NUMBER_OF_TIMES);
+    void playSong (int freqArray [], int timeArray[], int NUMBER_OF_NOTES);
  
     return 0;
 }
  
-bool Beep (double freqArray[], double song){
-    cout << "The Song is Playing! " << endl;
-    for (int play = 0; play < song; play++)
-    {
-        cout << freqArray[play];
-        cout << endl;
+void playSong (int freqArray [], int timeArray[], int NUMBER_OF_NOTES){
+    for (int play = 0; play < NUMBER_OF_NOTES; play++) {
+        Beep (freqArray[play], timeArray [play]);
     }
-    return true;
-};
+}
