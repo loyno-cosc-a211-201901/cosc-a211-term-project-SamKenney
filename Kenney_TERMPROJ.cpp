39fccc5dbde16df09271d8dc7e30e9b812b672ab
SamKenney <47219375+SamKenney@users.noreply.github.com>
2019-12-11 16:51:53 -0600

    Update Kenney_TERMPROJ.cpp
    Go

--- Kenney_TERMPROJ.cpp
+++ Kenney_TERMPROJ.cpp
@@ -2,9 +2,9 @@
 #include <windows.h>
 using namespace std;
  
-const int NUMBER_OF_NOTES = 117; //117
+const int NUMBER_OF_NOTES = 117; 
 const int NUMBER_OF_TIMES = 117;
-//const int NUMBER_OF_SLEEP = 117;
+const int NUMBER_OF_SLEEP = 117;
 const int NOTE_E5 = 659;
 const int NOTE_FS5 = 740;
 const int NOTE_CS4 = 277;
@@ -14,39 +14,65 @@ const int NOTE_D4 = 294;
 const int NOTE_E4 = 330;
 const int NOTE_B4 = 494;
 const int NOTE_B5 = 988;
-int play;
+const int Q_NOTE = 400;
+const int SOMETHING_NOTE = 1000;
+const int Half_NOTE = 1600;
+const int Whole_NOTE = 3200;
+
+
  
  
  
 void playSong (int freqArray [], int timeArray[],  int NUMBER_OF_NOTES);
 
-//void rest(int sleepArray []);
+//void rest (int timeArray[]);
+
  
 int main(){
     cout << "Welcome to the Christmas Music Box!" << endl;
     
     int  freqArray[NUMBER_OF_NOTES] = { NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_CS4, NOTE_A5, NOTE_FS5, NOTE_E5, NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_A5, NOTE_GS5, NOTE_D4, NOTE_E4, NOTE_D4, NOTE_B4, NOTE_GS5, NOTE_FS5, NOTE_E5, NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_FS5, NOTE_CS4,  NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_CS4, NOTE_A5, NOTE_FS5, NOTE_E5, NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_A5, NOTE_GS5, NOTE_D4, NOTE_E4, NOTE_D4, NOTE_B4, NOTE_GS5, NOTE_FS5, NOTE_E5, NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_B5, NOTE_A5, NOTE_FS5, NOTE_FS5, NOTE_A5, NOTE_FS5, NOTE_E5, NOTE_CS4, NOTE_E5, NOTE_D4, NOTE_FS5, NOTE_E5, NOTE_D4, NOTE_CS4, NOTE_B4, NOTE_CS4, NOTE_E4, NOTE_FS5, NOTE_GS5, NOTE_GS5, NOTE_GS5, NOTE_A5, NOTE_A5, NOTE_GS5, NOTE_FS5, NOTE_E4, NOTE_D4, NOTE_B4, NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_CS4, NOTE_A5, NOTE_FS5, NOTE_E5, NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_A5, NOTE_GS5, NOTE_D4, NOTE_E4, NOTE_D4, NOTE_B4, NOTE_GS5, NOTE_FS5, NOTE_E5, NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_B5, NOTE_A5, NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_FS5, NOTE_E5, NOTE_B5, NOTE_A5 };
     
-    int  timeArray[NUMBER_OF_TIMES] = { 400, 400, 400, 400, 400, 400, 1600, 400, 400, 400, 400, 400, 400, 1600, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500 };
+    int  timeArray[NUMBER_OF_TIMES] = { Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE, Q_NOTE };
 
-   // int sleepArray[NUMBER_OF_SLEEP] = { 500, 500, 500, 500, 300, 300, 300 };
+
  
     playSong (freqArray, timeArray, NUMBER_OF_NOTES);
+    
+    //rest (timeArray);
 
-    //rest(sleepArray);
+    
+   cout << "All Done" << endl;
+
+
  
     return 0;
 }
  
 void playSong (int freqArray [], int timeArray[],  int NUMBER_OF_NOTES){
-    for (int play = 0; play < NUMBER_OF_NOTES; play++) {
-        Beep (freqArray[play], timeArray[play]);
+
+    int play;
+
+    for (play = 0; play < NUMBER_OF_NOTES; play++) {    
+        cout << freqArray[play] << endl;
+        cout << "The play counter is now: " << play << endl;
+        Beep (freqArray[play], timeArray[play]); 
+        Sleep(timeArray[play]); 
     }
+
+    cout << freqArray[play] << endl;
+    cout << "The play counter is now: " << play << endl;
+    Beep (NOTE_A5, Q_NOTE);
+    Sleep(timeArray[Q_NOTE]); 
 }
 
-//void rest(int sleepArray []) {
-  //  for (play = 0; play < NUMBER_OF_SLEEP; play++) {
-    //    Sleep(sleepArray[play]);
-    //}
-    
+//void rest (int timeArray[]){
+
+  //  int pause;
+
+    //for (pause = 0; pause < NUMBER_OF_TIMES; pause++){
+      //  cout << "Sleep" << endl; 
+        //Sleep(timeArray[pause]);
+   // }
 //}
+
